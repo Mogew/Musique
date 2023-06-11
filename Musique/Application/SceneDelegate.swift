42d1c4252d7a739cf4ef137f6ae2a9683e23e22a
jstted <tedeev17@gmail.com>
2023-06-11 19:28:12 +0300

    update folders, remove trash

---
+++ Musique/Application/SceneDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    //MARK: - Property
+    var window: UIWindow?
+
+    //MARK: - Methods
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.overrideUserInterfaceStyle = .dark
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
+}
