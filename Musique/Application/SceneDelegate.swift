3ea2ffaaf4844b661293b6e1dc17f150a6ac3c1e
Olga <olga.shovgeneva@gmail.com>
2023-06-24 18:09:35 +0300

    add ui

--- Musique/Application/SceneDelegate.swift
+++ Musique/Application/SceneDelegate.swift
@@ -10,7 +10,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.overrideUserInterfaceStyle = .dark
-        window?.rootViewController = TabBarController()
+        window?.rootViewController = EditViewController()
         window?.makeKeyAndVisible()
     }
 }
--- Musique/Modules/Edit/EditViewController.swift
+++ Musique/Modules/Edit/EditViewController.swift
@@ -14,6 +14,11 @@ class EditViewController: UIViewController {
     private var avatarView = UIView()
     private var avatarImageView = UIImageView()
     private var editAvatarButton = UIButton()
+    private var verticlaStackView = UIStackView()
+    private var editUsernameButton = EditButton()
+    private var editEmailButton = EditButton()
+    private var editGenderButton = EditButton()
+    private var editDateOfBirthButton = EditButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +30,90 @@ class EditViewController: UIViewController {
     //MARK: - Setup UI Elements
     
     private func setupViews() {
-        view.backgroundColor = .mBlack
+        view.backgroundColor = .mDarkBlue
         setupChangePasswordButton()
         setupBackgroundView()
         setupAvatarView()
         setupAvatarImageView()
         setupEditAvatarButton()
+        setupVerticlaStackView()
+        setupEditUsernameButton()
+        setupEditEmailButton()
+        setupEditGenderButton()
+        setupEditDateOfBirthButton()
     }
     
+    private func setupVerticlaStackView() {
+        verticlaStackView.axis = .vertical
+        verticlaStackView.alignment = .fill
+        verticlaStackView.distribution = .fillEqually
+        verticlaStackView.spacing = 22
+    }
+    
+    private func setupEditUsernameButton() {
+        editUsernameButton.setTitle1("Username")
+        editUsernameButton.setTitle2("John Huston")
+        editUsernameButton.addTarget(self, action: #selector(showEditUsernameAlert), for: .touchUpInside)
+            }
+    
+    @objc private func showEditUsernameAlert() {
+        let alert = UIAlertController(title: "Edit username",
+                                      message: "Enter a new username",
+                                      preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = "Username"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Safe", style: .default, handler: { _ in
+            //read textfield value
+        }))
+
+        present(alert, animated: true)
+    }
+    
+    private func setupEditEmailButton() {
+        editEmailButton.setTitle1("Email")
+        editEmailButton.setTitle2("Mufillluma@gmail.com")
+        editEmailButton.addTarget(self, action: #selector(showEditEmailAlert), for: .touchUpInside)
+    }
+    
+    @objc private func showEditEmailAlert() {
+        let alert = UIAlertController(title: "Edit email",
+                                      message: "Enter a new email",
+                                      preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = "Username"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Safe", style: .default, handler: { _ in
+            //read textfield value
+        }))
+
+        present(alert, animated: true)
+    }
+    
+    private func setupEditDateOfBirthButton() {
+        editDateOfBirthButton.setTitle1("Date of birth")
+        editDateOfBirthButton.setTitle2("10/09/1995")
+        editEmailButton.addTarget(self, action: #selector(showEditDateOfBirthAlert), for: .touchUpInside)
+    }
+    
+    @objc private func showEditDateOfBirthAlert() {
+
+    }
+    
+    private func setupEditGenderButton() {
+        editGenderButton.setTitle1("Gender")
+        editGenderButton.setTitle2("Male")
+        editUsernameButton.addTarget(self, action: #selector(showGenderUsernameAlert), for: .touchUpInside)
+    }
+    
+    @objc private func showGenderUsernameAlert() {
+        
+    }
+
     private func setupAvatarView() {
         avatarView.backgroundColor = .clear
     }
@@ -41,6 +122,13 @@ class EditViewController: UIViewController {
         editAvatarButton.setImage(UIImage(named: "editImageButtonImage.png"), for: .normal)
         editAvatarButton.layer.cornerRadius = 20
         editAvatarButton.clipsToBounds = true
+        editAvatarButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    @objc func buttonTapped() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true, completion: nil)
     }
     
     private func setupChangePasswordButton() {
@@ -51,7 +139,7 @@ class EditViewController: UIViewController {
     }
     
     private func setupBackgroundView() {
-        backgroundView.backgroundColor = .mGray
+        backgroundView.backgroundColor = UIColor(red: 0.54, green: 0.57, blue: 0.66, alpha: 1)
         backgroundView.layer.cornerRadius = 12
         backgroundView.clipsToBounds = false
 //        backgroundView.layer.shadowColor = UIColor.mWhite.cgColor
@@ -63,9 +151,11 @@ class EditViewController: UIViewController {
         avatarImageView.image = UIImage(named: "baseAvatarImage.png")
         avatarImageView.layer.cornerRadius = 71
         avatarImageView.layer.borderWidth = 3
-        avatarImageView.layer.borderColor = UIColor.systemGray.cgColor // выбрать правильный цвет
+        avatarImageView.layer.borderColor = UIColor(red: 173, green: 176, blue: 192, alpha: 1).cgColor
         avatarImageView.clipsToBounds = true
     }
+    
+    
     //MARK: - Layout
     
     private func setConstraints() {
@@ -79,6 +169,11 @@ class EditViewController: UIViewController {
         backgroundView.addSubview(avatarView)
         avatarView.addSubview(avatarImageView)
         avatarView.addSubview(editAvatarButton)
+        backgroundView.addSubview(verticlaStackView)
+        verticlaStackView.addArrangedSubview(editUsernameButton)
+        verticlaStackView.addArrangedSubview(editEmailButton)
+        verticlaStackView.addArrangedSubview(editGenderButton)
+        verticlaStackView.addArrangedSubview(editDateOfBirthButton)
     }
     
     private func makeConstraints() {
@@ -118,5 +213,22 @@ class EditViewController: UIViewController {
             editAvatarButton.bottomAnchor.constraint(equalTo: avatarView.bottomAnchor, constant: 0),
             editAvatarButton.trailingAnchor.constraint(equalTo: avatarView.trailingAnchor, constant: 0)
         ])
+        verticlaStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            verticlaStackView.topAnchor.constraint(equalTo: avatarView.bottomAnchor, constant: 90),
+            verticlaStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 15),
+            verticlaStackView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -15),
+            verticlaStackView.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: -88)
+        ])
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension EditViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        avatarImageView.image = image
     }
 }
